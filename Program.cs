da3430c65dd15523f829cee2f6b8bf0d4c6371e3
Yuri Oliveira <yuri_souzaln@outlook.com>
2025-05-04 11:19:07 -0300

    Hi mom

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Drawing;
+using System.Runtime.InteropServices;
+using static System.Net.Mime.MediaTypeNames;
+
+class Program
+{
+    [DllImport("user32.dll")]
+    public static extern IntPtr GetDC(IntPtr hwnd);
+    public static void Main()
+    {
+        IntPtr desktop = GetDC(IntPtr.Zero);
+
+        using (Graphics g = Graphics.FromHdc(desktop))
+        {
+            System.Drawing.Font font = new System.Drawing.Font("Courier New", 60);
+            string text = "I'M THE CREEPER. CATCH ME IF YOU CAN!";
+
+            // Medir o tamanho do texto
+            SizeF textSize = g.MeasureString(text, font);
+
+            // Calcular a posição centralizada
+            float x = (1920 - textSize.Width) / 2;
+            float y = (1080 - textSize.Height) / 2;
+
+
+            while (true)
+            {
+                g.FillRectangle(Brushes.Black, 0, 0, 1920, 1080);
+                g.DrawString(text, font, Brushes.Green, x, y);
+                g.DrawString(text, font, Brushes.Green, x, y);
+            }
+        }
+    }
+}
